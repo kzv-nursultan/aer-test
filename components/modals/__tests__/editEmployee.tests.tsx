18c7d1267ced69c4630eac076aa3d5f635676d57
kzv-nursultan <kazievnursultan5554@gmail.com>
2024-05-09 02:29:05 +0600

    added edit employee modal tests

---
+++ components/modals/__tests__/editEmployee.tests.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { mockTestProvider } from "@/helpers/mockProviders";
+import userEvent from "@testing-library/user-event";
+import EditEmployee from "../editEmployee";
+
+const mockEmployee = {
+  id: 1,
+  name: "",
+  age: 0,
+  email: "",
+  position: "",
+  department: "",
+};
+
+describe("test edit employee data modal", () => {
+  //beforeAll(() => render(mockTestProvider(<EditEmployee employee={mockEmployee} />)));
+  it("should make sure that edit button exist", async () => {
+    render(mockTestProvider(<EditEmployee employee={mockEmployee} />));
+    const editButton = await screen.findByRole("button");
+    await userEvent.click(editButton);
+    expect(editButton).toBeEnabled();
+  });
+  it("should be able to add text to the input", async () => {
+    const { container } = render(
+      mockTestProvider(<EditEmployee employee={mockEmployee} />)
+    );
+    const editButton = await screen.findByRole("button");
+    await userEvent.click(editButton);
+    const nameInput = container.querySelector("#id-for-name-input");
+    await userEvent.type(nameInput as Element, "employee");
+    expect(nameInput).toHaveValue("employee");
+  });
+});
